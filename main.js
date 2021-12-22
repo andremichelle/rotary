0fb6f5ca6bb59850e98d01b845a15edd4a39d71a
andremichelle <andre.michelle@gmail.com>
2021-12-22 17:27:48 +0100

    length variations and stackable model

--- main.js
+++ main.js
@@ -1,130 +1,97 @@
 const WHITE = "white"
 const TRANSPARENT = "rgba(255, 255, 255, 0.0)"
 const PI = Math.PI
-const PI_HALF = PI / 2.0
 const TAU = PI * 2.0
+const FILL_FLAT = 0
+const FILL_POSITIVE = 1
+const FILL_NEGATIVE = 2
+const wrap = (value, bound) => {
+    value %= bound
+    return value < 1e-13 ? value + bound : value
+}
+const MoveFunction = Object.freeze({
+    Reverse: fx => x => 1.0 - fx(x),
+    AccAndStop: exp => x => Math.pow(x - Math.floor(x), exp),
+    StopAndGo: x => 1.0 - Math.min(1.0, 2.0 * (2.0 * x - Math.floor(2.0 * x))),
+    Sine: x => Math.sin(x * PI),
+    OddShape: shape => {
+        // https://www.desmos.com/calculator/bpbuua3l0j
+        const o = Math.pow(2.0, shape)
+        const c = Math.pow(2.0, o - 1)
+        return x => c * Math.sign(x - 0.5) * Math.pow(Math.abs(x - 0.5), o) + 0.5
+    }
+})
 
-let frame = -60
+const MOVEMENTS = [
+    MoveFunction.StopAndGo,
+    MoveFunction.Sine,
+    MoveFunction.AccAndStop(2.0),
+    MoveFunction.AccAndStop(3.0),
+    MoveFunction.OddShape(-1.0),
+    MoveFunction.OddShape(1.0),
+    MoveFunction.OddShape(2.0),
+]
+
+let frame = 0
 ;
 
-class RotarySegment {
-    constructor(radiusMin, radiusMax, angleMin = 0.0, angleMax = TAU, fill = RotarySegment.FILL_FLAT) {
-        console.assert(radiusMin < radiusMax, "radiusMax must be greater then radiusMin")
-        console.assert(angleMin < angleMax, "angleMax must be greater then angleMin")
 
-        this.radiusMin = radiusMin
-        this.radiusMax = radiusMax
-        this.angleMin = angleMin
-        this.angleMax = angleMax
+class RotaryTrack {
+    constructor(numSegments, width, widthRatio, length, lengthRatio, fill, moveFunc, reverse) {
+        this.numSegments = numSegments
+        this.width = width
+        this.widthRatio = widthRatio
+        this.length = length
+        this.lengthRatio = lengthRatio
         this.fill = fill
-
-        this.rotation = 0.0
+        this.reverse = reverse
+        this.moveFunc = moveFunc
+        this.phase = 0.0
+        this.phaseOffset = 0.0
     }
 
-    draw(context) {
-        const wrap = (value, bound) => {
-            value %= bound
-            return value < 1e-13 ? value + bound : value
+    rotateTo(value) {
+        this.phase = (this.moveFunc(value - Math.floor(value))) * (this.reverse ? -1 : 1) + this.phaseOffset
+    }
+
+    draw(context, radius) {
+        const scale = this.length / this.numSegments
+        const radiusMax = radius + Math.max(this.width * this.widthRatio, 1.0)
+        for (let i = 0; i < this.numSegments; i++) {
+            const angleMin = i * scale + this.phase
+            const angleMax = angleMin + scale * this.lengthRatio
+            this.drawSection(context, radius, radiusMax, angleMin, angleMax, this.fill)
         }
-        const ad = this.rotation * TAU;
-        const a0 = this.angleMin + ad;
-        const a1 = this.angleMax + ad;
+    }
+
+    drawSection(context, radiusMin, radiusMax, angleMin, angleMax, fill) {
+        console.assert(radiusMin < radiusMax, `radiusMax(${radiusMax}) must be greater then radiusMin(${radiusMin})`)
+        console.assert(angleMin < angleMax, `angleMax(${angleMax}) must be greater then angleMin(${angleMin})`)
+
+        const a0 = angleMin
+        const a1 = angleMax
         const gradient = context.createConicGradient(a0, 0.0, 0.0)
-        if (this.fill === RotarySegment.FILL_FLAT) {
+        if (fill === FILL_FLAT) {
             context.fillStyle = WHITE
         } else {
-            const offset = Math.min(wrap(Math.abs(a1 - a0), TAU) / TAU, 1.0)
-            if (this.fill === RotarySegment.FILL_POSITIVE) {
+            const offset = Math.min(wrap(Math.abs(a1 - a0), 1.0), 1.0)
+            if (fill === FILL_POSITIVE) {
                 gradient.addColorStop(0.0, TRANSPARENT)
                 gradient.addColorStop(offset, WHITE)
                 gradient.addColorStop(offset, TRANSPARENT) // eliminates tiny glitches at the end of the tail
-            } else {
+            } else if (fill === FILL_NEGATIVE) {
                 gradient.addColorStop(0.0, WHITE)
                 gradient.addColorStop(offset, TRANSPARENT)
             }
             context.fillStyle = gradient
         }
         context.beginPath()
-        context.arc(0.0, 0.0, this.radiusMax, a0, a1, false)
-        context.arc(0.0, 0.0, this.radiusMin, a1, a0, true)
+        context.arc(0.0, 0.0, radiusMax, a0 * TAU, a1 * TAU, false)
+        context.arc(0.0, 0.0, radiusMin, a1 * TAU, a0 * TAU, true)
         context.closePath()
         context.fill()
     }
 }
-
-RotarySegment.FILL_FLAT = 0;
-RotarySegment.FILL_POSITIVE = 1;
-RotarySegment.FILL_NEGATIVE = 2;
-
-/**
- * RotaryTrack properties:
- *  index in track-array
- *  numSegments
- *  width
- *  widthRatio
- *  radian
- *  radianRatio
- *  excludes?
- *  direction
- *  movement
- *  fill
- */
-
-class RotaryTrack {
-    // TODO Define r0, r1 in runtime by accumulating track-widths
-    constructor(numSegments, r0, r1, widthRatio = 1.0, lengthRatio = 0.5, fill = RotarySegment.FILL_POSITIVE) {
-        this.segments = []
-
-        this.sign = Math.sign(Math.random() * 2.0 - 1.0)
-        this.movement = RotaryTrack.MOVEMENTS[Math.floor(Math.random() * RotaryTrack.MOVEMENTS.length)]
-
-        const scale = TAU / numSegments
-        for (let i = 0; i < numSegments; i++) {
-            const a0 = i * scale
-            const a1 = a0 + scale * lengthRatio
-            const radius = (r0 + r1) * 0.5
-            const widthHalf = Math.max((r1 - r0) * 0.5 * widthRatio, 0.5)
-            if (Math.random() < 0.9) {
-                this.segments.push(new RotarySegment(radius - widthHalf, radius + widthHalf, a0, a1, fill))
-            }
-        }
-    }
-
-    rotateTo(value) {
-        value -= Math.floor(value)
-        value = this.movement(value)
-        for (let i = 0; i < this.segments.length; i++) {
-            this.segments[i].rotation = value * this.sign
-        }
-    }
-
-    draw(context) {
-        for (let i = 0; i < this.segments.length; i++) {
-            this.segments[i].draw(context)
-        }
-    }
-}
-
-RotaryTrack.AccAndStop = exp => x => Math.pow(x - Math.floor(x), exp)
-RotaryTrack.StopAndGo = x => 1.0 - Math.min(1.0, 2.0 * (2.0 * x - Math.floor(2.0 * x)))
-RotaryTrack.Sine = x => Math.sin(x * PI)
-RotaryTrack.OddShape = shape => {
-    // https://www.desmos.com/calculator/bpbuua3l0j
-    const o = Math.pow(2.0, shape)
-    const c = Math.pow(2.0, o - 1)
-    return x => c * Math.sign(x - 0.5) * Math.pow(Math.abs(x - 0.5), o) + 0.5
-}
-
-RotaryTrack.MOVEMENTS = [
-    RotaryTrack.StopAndGo,
-    RotaryTrack.Sine,
-    RotaryTrack.AccAndStop(2.0),
-    RotaryTrack.AccAndStop(3.0),
-    RotaryTrack.OddShape(-2.0),
-    RotaryTrack.OddShape(-1.0),
-    RotaryTrack.OddShape(1.0),
-    RotaryTrack.OddShape(2.0),
-]
 
 class Rotary {
     constructor(tracks) {
@@ -138,8 +105,11 @@ class Rotary {
     }
 
     draw(context) {
+        let radius = 24
         for (let i = 0; i < this.tracks.length; i++) {
-            this.tracks[i].draw(context)
+            const track = this.tracks[i];
+            track.draw(context, radius)
+            radius += track.width
         }
     }
 }
@@ -149,13 +119,13 @@ class RotaryBuilder {
         const tracks = []
         for (let i = 0; i < 12; i++) {
             const numSegments = 1 + Math.floor(Math.random() * 9)
-            const r0 = 24 + 12 * i
             if (Math.random() < 0.1) ++i
-            const r1 = 24 + 12 * (i + 1)
             const lengthRatioExp = -Math.floor(Math.random() * 3)
             const lengthRatio = 0 === lengthRatioExp ? 1.0 : Math.random() < 0.5 ? 1.0 - Math.pow(2.0, lengthRatioExp) : Math.pow(2.0, lengthRatioExp)
-            const fill = RotarySegment.FILL_FLAT
-            tracks.push(new RotaryTrack(0 === lengthRatioExp ? 1 : numSegments, r0, r1, Math.random(), lengthRatio, 2 === numSegments ? RotarySegment.FILL_POSITIVE : fill))
+            tracks.push(new RotaryTrack(
+                0 === lengthRatioExp ? 1 : numSegments,
+                12.0, Math.random(), Math.random() < 0.1 ? 0.75 : 1.0, lengthRatio, 2 === numSegments ? FILL_POSITIVE : FILL_FLAT,
+                MOVEMENTS[Math.floor(Math.random() * MOVEMENTS.length)], true))
         }
         return new Rotary(tracks)
     }
