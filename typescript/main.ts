9af536bedd76bcfb8e4ab108c53e0fa4f7577622
andremichelle <andre.michelle@gmail.com>
2021-12-24 13:19:59 +0100

    add TrackControls

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,7 +1,24 @@
-import {Rotary} from "./rotary"
+import {Rotary, RotaryTrack} from "./rotary"
 import {NumericStepper} from "./controls"
 import {PrintMapping} from "./common"
 
 
 const rotary = new Rotary()
+
+const tracksContainer = document.querySelector(".tracks")
+const trackTemplate = document.querySelector(".track")
+trackTemplate.remove()
+
 new NumericStepper(document.querySelector("[data-parameter='start-radius']"), rotary.radiusMin, PrintMapping.NoFloat, 1, "px")
+
+class RotaryTrackView {
+    constructor(private readonly element: HTMLElement, private readonly track: RotaryTrack) {
+    }
+}
+
+
+let trackViews = rotary.tracks.map(track => {
+    const element = trackTemplate.cloneNode(true) as HTMLElement
+    tracksContainer.appendChild(element)
+    return new RotaryTrackView(element, track)
+})
--- typescript/rotary.ts
+++ typescript/rotary.ts
@@ -14,8 +14,19 @@ export class RotaryTrack implements Terminable {
 
 export class Rotary implements Terminable {
     private readonly terminator: Terminator = new Terminator()
-    private readonly tracks: RotaryTrack[] = [new RotaryTrack()]
 
+    readonly tracks: RotaryTrack[] = [
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack(),
+        new RotaryTrack()
+    ]
     readonly radiusMin = this.terminator.with(new ObservableValue(new LinearInteger(0, 128), 20))
 
     terminate() {
